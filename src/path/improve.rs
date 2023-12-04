c23f84f2acfac1e6acdb4c76eb15b963d3deb0e8
leo blume <leonardo.blume@gymnasium-essen-werden.de>
2023-12-05 00:58:29 +0100

    Implement rotate correctly

--- src/path/improve.rs
+++ src/path/improve.rs
@@ -4,10 +4,14 @@ use crate::graph::{Path, Edge};
 
 pub fn rotate(_client: &Responder, dim: u8, old_path: Path) -> Path {
     let edges = old_path.clone().to_edges();
-    let (min_idx, min_edge) = edges.into_iter().enumerate().min_by(|(_,e1),(_,e2)|e1.dist().total_cmp(&e2.dist())).expect("should be nonempty");
-    if min_edge.dist() < Edge::new(old_path[0].clone(), old_path[old_path.len()-1].clone()).dist() {
+    let (max_idx, max_edge) = edges.into_iter().enumerate().max_by(|(_,e1),(_,e2)|e1.dist().total_cmp(&e2.dist())).expect("should be nonempty");
+    if max_edge.dist() > Edge::new(old_path[0].clone(), old_path[old_path.len()-1].clone()).dist() {
         let mut new_path = old_path.into_inner();
-        new_path.rotate_left(min_idx);
+        let len = new_path.len();
+        // So rotieren, dass min_idx auf -1 liegt.
+        let left = (max_idx +1) % len;
+        dbg!(len, left);
+        new_path.rotate_left(left);
         Path::try_new(new_path, dim).unwrap()
     } else {
         old_path
