07550fe2a0ae074df614a27604508dff79fa80aa
l blume <leonardo.blume@gymnasium-essen-werden.de>
2024-04-25 12:49:31 +0200

    Remove good_lp for lack of dependencies

--- src/path/create/ilp.rs
+++ src/path/create/ilp.rs
@@ -1,13 +1,13 @@
-use good_lp::coin_cbc;
-use good_lp::constraint::{eq, geq, leq};
-use good_lp::scip;
-use good_lp::variable;
-use good_lp::variable::UnsolvedProblem;
-use good_lp::variables;
-use good_lp::Expression;
-use good_lp::Solution;
-use good_lp::SolverModel;
-use good_lp::Variable;
+// use good_lp::coin_cbc;
+// use good_lp::constraint::{eq, geq, leq};
+// use good_lp::scip;
+// use good_lp::variable;
+// use good_lp::variable::UnsolvedProblem;
+// use good_lp::variables;
+// use good_lp::Expression;
+// use good_lp::Solution;
+// use good_lp::SolverModel;
+// use good_lp::Variable;
 use serde::Deserialize;
 use std::iter::once;
 
@@ -240,130 +240,130 @@ fn cycle_to_edges_symmetric(cycle: &[usize]) -> impl Iterator<Item = (usize, usi
     cycle_to_edges(cycle).chain(cycle_to_edges(cycle).map(|(a, b)| (b, a)))
 }
 
-pub fn solve_good<C: CreateContext>(ctx: C) -> C::Path {
-    let matrix: Matrix = ctx.adjacency_matrix();
+// pub fn solve_good<C: CreateContext>(ctx: C) -> C::Path {
+//     let matrix: Matrix = ctx.adjacency_matrix();
 
-    let size = ctx.len();
-    let node_indices = || (0..size);
+//     let size = ctx.len();
+//     let node_indices = || (0..size);
 
-    let pool = ctx.options();
+//     let pool = ctx.options();
 
-    variables! { problem: };
+//     variables! { problem: };
 
-    let mut x = Vec::with_capacity(size);
-    for i in node_indices() {
-        let mut matrix_row = Vec::with_capacity(size);
-        for j in node_indices() {
-            matrix_row.push(problem.add(variable().binary().name(format!("x_{i},{j}"))))
-        }
-        x.push(matrix_row);
-    }
-    let x = x;
+//     let mut x = Vec::with_capacity(size);
+//     for i in node_indices() {
+//         let mut matrix_row = Vec::with_capacity(size);
+//         for j in node_indices() {
+//             matrix_row.push(problem.add(variable().binary().name(format!("x_{i},{j}"))))
+//         }
+//         x.push(matrix_row);
+//     }
+//     let x = x;
 
-    let solution = problem.minimise(
-        node_indices()
-            .map(|i| {
-                node_indices()
-                    .map(|j| x[i][j] * matrix[(i, j)])
-                    .sum::<Expression>()
-            })
-            .sum::<Expression>(),
-    );
+//     let solution = problem.minimise(
+//         node_indices()
+//             .map(|i| {
+//                 node_indices()
+//                     .map(|j| x[i][j] * matrix[(i, j)])
+//                     .sum::<Expression>()
+//             })
+//             .sum::<Expression>(),
+//     );
 
-    match pool.milp_solver.unwrap_or_default() {
-        MilpSolver::CoinOrCbc => solve_generic(coin_cbc, solution, ctx, x),
-        MilpSolver::Scip => solve_generic(scip, solution, ctx, x),
-    }
-}
+//     match pool.milp_solver.unwrap_or_default() {
+//         MilpSolver::CoinOrCbc => solve_generic(coin_cbc, solution, ctx, x),
+//         MilpSolver::Scip => solve_generic(scip, solution, ctx, x),
+//     }
+// }
 
-pub fn solve_generic<M, C: CreateContext>(
-    solver: fn(UnsolvedProblem) -> M,
-    problem: UnsolvedProblem,
-    ctx: C,
-    x: Vec<Vec<Variable>>,
-) -> C::Path
-where
-    M: SolverModel,
-{
-    let size = ctx.len();
-    let node_indices = || (0..size);
+// pub fn solve_generic<M, C: CreateContext>(
+//     solver: fn(UnsolvedProblem) -> M,
+//     problem: UnsolvedProblem,
+//     ctx: C,
+//     x: Vec<Vec<Variable>>,
+// ) -> C::Path
+// where
+//     M: SolverModel,
+// {
+//     let size = ctx.len();
+//     let node_indices = || (0..size);
 
-    let mut model = problem.using(solver);
+//     let mut model = problem.using(solver);
 
-    // Jede Zeile darf maximal einen Eintrag haben.
-    for i in node_indices() {
-        model = model.with(leq(
-            node_indices().map(|j| x[i][j]).sum::<Expression>(),
-            1.0,
-        ));
-    }
+//     // Jede Zeile darf maximal einen Eintrag haben.
+//     for i in node_indices() {
+//         model = model.with(leq(
+//             node_indices().map(|j| x[i][j]).sum::<Expression>(),
+//             1.0,
+//         ));
+//     }
 
-    // Jede Spalte darf maximal einen Eintrag haben.
-    for j in node_indices() {
-        model = model.with(leq(
-            node_indices().map(|i| x[i][j]).sum::<Expression>(),
-            1.0,
-        ));
-    }
+//     // Jede Spalte darf maximal einen Eintrag haben.
+//     for j in node_indices() {
+//         model = model.with(leq(
+//             node_indices().map(|i| x[i][j]).sum::<Expression>(),
+//             1.0,
+//         ));
+//     }
 
-    // Jeder Knoten muss erreicht werden.
-    for i in node_indices() {
-        model = model.with(geq(
-            node_indices()
-                .map(|j| x[i][j] + x[j][i])
-                .sum::<Expression>(),
-            1.0,
-        ));
-    }
+//     // Jeder Knoten muss erreicht werden.
+//     for i in node_indices() {
+//         model = model.with(geq(
+//             node_indices()
+//                 .map(|j| x[i][j] + x[j][i])
+//                 .sum::<Expression>(),
+//             1.0,
+//         ));
+//     }
 
-    // Keine Hin- und Rückknoten.
-    for i in node_indices() {
-        for j in node_indices().skip(i) {
-            model = model.with(leq(x[i][j] + x[j][i], 1.0))
-        }
-    }
+//     // Keine Hin- und Rückknoten.
+//     for i in node_indices() {
+//         for j in node_indices().skip(i) {
+//             model = model.with(leq(x[i][j] + x[j][i], 1.0))
+//         }
+//     }
 
-    // Insgesamt werden n-1 Knoten erreicht.
-    model = model.with(eq(
-        node_indices()
-            .map(|i| node_indices().map(|j| x[i][j]).sum::<Expression>())
-            .sum::<Expression>(),
-        (size - 1) as f64,
-    ));
+//     // Insgesamt werden n-1 Knoten erreicht.
+//     model = model.with(eq(
+//         node_indices()
+//             .map(|i| node_indices().map(|j| x[i][j]).sum::<Expression>())
+//             .sum::<Expression>(),
+//         (size - 1) as f64,
+//     ));
 
-    let solution = model.solve().expect("no solution found");
+//     let solution = model.solve().expect("no solution found");
 
-    for i in node_indices() {
-        for j in node_indices() {
-            dbg!(solution.value(x[i][j]));
-        }
-    }
+//     for i in node_indices() {
+//         for j in node_indices() {
+//             dbg!(solution.value(x[i][j]));
+//         }
+//     }
 
-    // let _paths = print_paths_solution_generic(solution, model, &x);
+//     // let _paths = print_paths_solution_generic(solution, model, &x);
 
-    todo!()
-}
+//     todo!()
+// }
 
-fn print_paths_solution_generic<S: Solution, M: SolverModel>(
-    solution: S,
-    model: M,
-    x: &Vec<Vec<Variable>>,
-) -> Vec<Vec<usize>> {
-    let paths = edges_to_paths({
-        let solution = &solution;
-        let x = &x;
-        &(0..x.len())
-            .flat_map(move |i| {
-                (0..x.len())
-                    .map(move |j| (i, j, solution.value(x[i][j])))
-                    .filter(|&(_, _, v)| v == 1.0)
-                    .map(|(i, j, _)| (i, j))
-            })
-            .collect_vec()
-    });
-    for path in paths.iter() {
-        println!("{}", path.iter().join(" -> "));
-    }
-    println!();
-    paths
-}
+// fn print_paths_solution_generic<S: Solution, M: SolverModel>(
+//     solution: S,
+//     model: M,
+//     x: &Vec<Vec<Variable>>,
+// ) -> Vec<Vec<usize>> {
+//     let paths = edges_to_paths({
+//         let solution = &solution;
+//         let x = &x;
+//         &(0..x.len())
+//             .flat_map(move |i| {
+//                 (0..x.len())
+//                     .map(move |j| (i, j, solution.value(x[i][j])))
+//                     .filter(|&(_, _, v)| v == 1.0)
+//                     .map(|(i, j, _)| (i, j))
+//             })
+//             .collect_vec()
+//     });
+//     for path in paths.iter() {
+//         println!("{}", path.iter().join(" -> "));
+//     }
+//     println!();
+//     paths
+// }
